a1a15e45f0ecff07041e3eeecabc23153ebb7641
Alex Kiborgok <borgokalek14@gmail.com>
2022-03-19 12:34:16 +0300

    add function to get toppings values

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 $(document).ready(function () {
-    $(".label-size").click(function () {
-        $("input[name='size']").removeProp("selected");
-        $(this).find("input[name='size']").prop("selected", true);
-        console.log($("input[name='size']:selected").val());
-    })
-})
+  //Function to get size values
+  $(".label-size").click(function () {
+    $("input[name='size']").removeProp("selected");
+    $(this).find("input[name='size']").prop("selected", true);
+    console.log($("input[name='size']:selected").val());
+  });
+  //Function to get crust values
+  $(".crust").click(function () {
+    $("input[name='crust']").removeProp("selected");
+    $(this).find("input[name='crust']").prop("selected", true);
+    console.log($("input[name='crust']:selected").val());
+  });
+  //Function to get toppings values
+
+  $("#btncheck1").click(function () {
+    $(this).prop("checked", $(!this).prop("checked"));
+    if ($(this).is(":checked")) {
+      console.log($(this).val());
+    }
+  });
+  $("#btncheck2").click(function () {
+    $(this).prop("checked", $(!this).prop("checked"));
+    if ($(this).is(":checked")) {
+      console.log($(this).val());
+    }
+  });
+  $("#btncheck3").click(function () {
+    $(this).prop("checked", $(!this).prop("checked"));
+    if ($(this).is(":checked")) {
+      console.log($(this).val());
+    }
+  });
+});
